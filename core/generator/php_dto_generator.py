12ccf4b5ea1d7895a3f00970078eb6833b481cb8
Joel <joel.2005.2@gmail.com>
2025-12-21 21:08:16 -0300

    Correccion

--- core/generator/php_dto_generator.py
+++ core/generator/php_dto_generator.py
@@ -2,16 +2,43 @@ from pathlib import Path
 from core.generator.base_generator import BaseGenerator
 
 class PhpDtoGenerator(BaseGenerator):
+
+    TYPE_MAP = {
+        "int": "int",
+        "string": "string",
+        "bool": "bool",
+        "float": "float",
+        "datetime": "\\DateTimeImmutable",
+    }
+
     def __init__(self):
         super().__init__("core/templates/php")
+
+    def normalize_fields(self, fields: list) -> list:
+        normalized = []
+
+        for field in fields:
+            raw_type = field["type"]
+
+            if raw_type not in self.TYPE_MAP:
+                raise ValueError(f"Unsupported PHP type: {raw_type}")
+
+            normalized.append({
+                "name": field["name"],
+                "type": self.TYPE_MAP[raw_type],
+            })
+
+        return normalized
 
     def generate(self, contract: dict, output_dir: str):
         template = self.load_template("dto.php.tpl")
 
+        fields = self.normalize_fields(contract["fields"])
+
         context = {
             "namespace": "App",
             "class_name": contract["entity"]["name"] + "Dto",
-            "fields": contract["fields"],
+            "fields": fields,
         }
 
         content = self.render(template, context)
